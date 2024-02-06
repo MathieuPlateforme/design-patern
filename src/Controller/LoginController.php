e4d8041ef15ebae21a40259b9393d6cc65050e9f
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2024-02-06 11:38:46 +0100

    Factorizing Controllers

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,4 +5,50 @@ namespace App\Controller;
 use App\Controller\Controller;
 
 class LoginController extends Controller
-{}
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function loginUser($email, $password)
+    {
+        $user = new User();
+
+        if (empty($email) || empty($password)) {
+            throw new \Exception("Tous les champs sont obligatoires");
+            $this->redirect('login');
+
+            return;
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \Exception("L'email n'est pas valide");
+            $this->redirect('login');
+
+            return;
+        }
+
+        $user = $user->findOneByEmail($email);
+
+        if ($user && password_verify($password, $user->getPassword())) {
+            $user->setPassword('');
+            $_SESSION['user'] = $user;
+
+            $this->redirect('home');
+
+            return;
+        } else {
+            throw new \Exception("Les identifiants sont incorects");
+            $this->redirect('login');
+
+            return;
+        }
+    }
+
+    public function logoutUser()
+    {
+        unset($_SESSION['user']);
+        $this->redirect('home');
+    }
+}
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,4 +5,30 @@ namespace App\Controller;
 use App\Controller\Controller;
 
 class ProfileController extends Controller
-{}
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function profile()
+    {
+        $user = new User();
+        if (self::getUser() === null) {
+            $this->redirect('login');
+
+            return;
+        }
+        $user = $user->findOneById($_SESSION['user']->getId());
+        if ($user) {
+            $user->setPassword('');
+            $this->render('profile', ['user' => $user]);
+
+            return;
+        }
+
+        $this->redirect('login');
+
+        return;
+    }
+}
